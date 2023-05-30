2c98467cd69a696cdc910599d4749014e29fe254
Brian Bassanetti <89162244+BrainTheBest5@users.noreply.github.com>
2023-05-30 12:25:07 +0200

    if as a one-liner check

--- Logic1.c
+++ Logic1.c
@@ -4,15 +4,9 @@ int main(){
 	int i;
 	printf("Insert a number: ");
 	scanf("%d",&i);
-	if(i>=15 && i<=18){
+	if((i>=15 && i<=18) || (i>=39 && i<=44) || (i>=92 && i<=102)){
 		printf("The number %d is valid\n",i);
  	}
-	else if(i>=39 && i<=44){
-		printf("The number %d is valid\n",i);
-        }
-	else if(i>=92 && i<=102){
-                printf("The number %d is valid\n",i);
-        }
 	else{
 		printf("The number %d is not valid\n",i);
         }
